470f68b1d0e43b5a2c289de618d1dba9a586e129
James Rogers-Wright <vivi870123@egmail.com>
2020-04-03 17:39:20 +0100

    added new apps to 'progs.csv'

---
+++ install_scripts/07-mount_partition.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. "$(dirname "$BASH_SOURCE")/../utils.sh"
+
+title "Configuring fstab to make windows partiion mount automatically"
+
+# create media folder
+sudo mkdir /media
+
+# give permission to user
+sudo chown $USER /media
+
+# create folder where the windows partition shold mount to
+mkdir /media/mydisk
+
+# mount partition
+sudo mount /dev/sda4 /media/mydisk
+
+# mount partition on boot
+$ su -
+echo "UUID=$(lsblk -no UUID /dev/sda4) /media/mydisk $(lsblk -no FSTYPE /dev/sda4) defaults,noatime 0 2" >> /etc/fstab
+mount -a
+
+progress "Finished!"
